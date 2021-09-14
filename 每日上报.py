81eb29267760c88fb690a2f97678d4074500a2b2
1173710216 <1105897758@qq.com>
2021-09-14 20:37:49 +0800

    Add another labels
    新增了除了kzl10之外的label

--- 每日上报.py
+++ 每日上报.py
@@ -4,13 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 
 print('初始化浏览器')
-USERNAME   = os.environ['ID']
-PASSWORD   = os.environ['PASSWORD']
-LOCATION   = os.environ['LOCATION']
-ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 MicroMessenger/6.3.27 NetType/WIFI Language/zh_CN'
+USERNAME = os.environ['ID']
+PASSWORD = os.environ['PASSWORD']
+ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 ' \
+     'MicroMessenger/6.3.27 NetType/WIFI Language/zh_CN '
+
 option = webdriver.ChromeOptions()
 option.headless = True
-option.add_argument('user-agent='+ua)
+option.add_argument('user-agent=' + ua)
 driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = option)
 
 print('正在上报')
@@ -20,18 +21,27 @@ driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
 driver.find_element_by_id('load').click()
 
 success = False
-for i in range (0, 5):
-	try:
-		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew')
-		driver.execute_script(f'kzl10 = "{LOCATION}"')
-		driver.execute_script('document.getElementById("txfscheckbox").click()')
-		driver.find_element_by_class_name('submit').click()
-		success = True
-		break
-	except:
-		print('失败' + str(i+1) + '次，正在重试...')
+for i in range(0, 5):
+    try:
+        driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew')
+        driver.execute_script(f'gpsjd = 126.633639')
+        driver.execute_script(f'gpswd = 45.743205')
+        driver.execute_script(f'kzl6 = "黑龙江省"')
+        driver.execute_script(f'kzl7 = "哈尔滨市"')
+        driver.execute_script(f'kzl8 = "南岗区"')
+        driver.execute_script(f'kzl9 = "工建街7号"')
+        driver.execute_script(f'kzl10 = "黑龙江省哈尔滨市南岗区花园街道哈尔滨工业大学西苑小区"')
+        driver.execute_script(f'kzl38 = "黑龙江省"')
+        driver.execute_script(f'kzl39 = "哈尔滨市"')
+        driver.execute_script(f'kzl39 = "南岗区"')
+        driver.execute_script('document.getElementById("txfscheckbox").click()')
+        driver.find_element_by_class_name('submit').click()
+        success = True
+        break
+    except:
+        print('失败' + str(i + 1) + '次，正在重试...')
 driver.quit()
 if success:
-	print('上报完成')
+    print('上报完成')
 else:
-	raise Exception('上报多次失败，可能学工系统已更新')
+    raise Exception('上报多次失败，可能学工系统已更新')
